5c9ed8486eb90a1187c072814751cc5af9c4be58
Andrew Ferrer <AndBFer@andrews-mbp.uconnect.utah.edu>
2017-11-06 20:30:54 -0700

    working on AJAX for initial search page

--- README.md
+++ README.md
@@ -8,5 +8,7 @@ TODO
 • enable search functionality
 • truncated search results, potentially with http://dotdotdot.frebsite.nl/
 • implement "branding" elements: images, logos, relevant & consistent visual cues
+• enable search functionality by creating a page that pulls results from firebase. This will recall recipe items, located in divs/etc., that are chosen through some selection logic in JS that relates the recipe to the search query. 
+
 
 
--- assets/js/search-home.js
+++ assets/js/search-home.js
@@ -1,15 +1,41 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 	$("#sizing-addon1").on("click", function(){
+
 		console.log("glyphicon clicked");
 		var query = $("#search-input").val().trim();
+
 		console.log(query);	
+		
 		var link = "https://whyisjacob.github.io/share_a_bite/recipe-list.html"
-		console.log(link);
 
 	// this might just be hardcoded, trying to avoid CORS issues
-	$(".container-fluid").load(link);
-	});
+	// $("#main-image").load(link);
+	// });
+	// Initialize Firebase
+		  // var config = {
+		  //   apiKey: "AIzaSyCgoRfIkQr6LbDgERECflwrP4Dmi2-TPKA",
+		  //   authDomain: "grab-a-bite-df6c2.firebaseapp.com",
+		  //   databaseURL: "https://grab-a-bite-df6c2.firebaseio.com",
+		  //   projectId: "grab-a-bite-df6c2",
+		  //   storageBucket: "grab-a-bite-df6c2.appspot.com",
+		  //   messagingSenderId: "915542502205"
+		  // };
 
+		  // firebase.initializeApp(config);
 
+//edamam api data
+var appId = "c26ba31b";
+var apiKey = "0e164b393b7346efa7e6769658022a14";
+var queryUrl = "https://api.edamam.com/search?q=" + query + "&app_id=${" + appId + "}&app_key=${" + apiKey + "}&from=0&to=6";
+ 
+$.ajax({
+      url: queryUrl,
+      method: "GET"
+    }).done(function(response) {
+      console.log(response);
+
+    });
+
+});
 });
