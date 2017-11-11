2789331df401d7c7d001a42f8bcd7df51777e9b8
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-11-10 22:33:24 -0700

    just getting it saved as I go

--- assets/js/search-index.js
+++ assets/js/search-index.js
@@ -8,15 +8,15 @@ $( document ).ready(function() {
 		console.log(query);	
 
 		var link = "https://whyisjacob.github.io/share_a_bite/recipe-list.html"
-
+		var devLink = "../recipe-list.html";
 	// this might just be hardcoded, trying to avoid CORS issues
-	$("#main-image").load(link);
+	$("#main-image").load(devLink).addClass(".for-hiding");
 	
 
 //edamam api data
 		var apeId = "c26ba31b";
 		var apiKey = "0e164b393b7346efa7e6769658022a14";
-		var queryUrl = "https://api.edamam.com/search?q=" + query + "&app_id=" + apeId + "&app_key=" + apiKey + "&from=0&to=6&callback=myfunc";
+		var queryUrl = "https://api.edamam.com/search?q=" + query + "&app_id=" + apeId + "&app_key=" + apiKey + "&from=0&to=3&callback=myfunc";
 		var myfunc = function (json) {
 		  alert(json);
 			};
@@ -37,9 +37,7 @@ $( document ).ready(function() {
 		    	var edamamZeroUrl = response.hits[0].recipe.url;
 		    	var edamamOneUrl = response.hits[1].recipe.url;
 		    	var edamamTwoUrl = response.hits[2].recipe.url;
-		    	var edamamThreeUrl = response.hits[3].recipe.url;
-		    	var edamamFourUrl = response.hits[4].recipe.url;
-		    	var edamamFiveUrl = response.hits[5].recipe.url;
+
 
 
 	
@@ -68,6 +66,11 @@ $( document ).ready(function() {
 
 		    		console.log(recipeZero);
 		    			$("#recipeZero").text(recipeZero.title);
+		    			$("#authorZero").text("Added By: " + recipeZero.author);
+		    			var rImage = $("<img>").attr("src", recipeZero.image).attr("alt", recipeZero.title).attr("target", "_blank").attr("data-ID", recipeZero.unique).css("cursor", "pointer");
+		    			rImage.addClass("rec-image");
+		    			$(".recipeImageZero").append(rImage);
+		    			$(".descriptionZero").append(recipeZero.description);
 					});
 
 
@@ -91,6 +94,12 @@ $( document ).ready(function() {
 		    		}
 
 		    		console.log(recipeOne);
+		    			$("#recipeOne").text(recipeOne.title);
+		    			$("#authorOne").text("Added By: " + recipeOne.author);
+		    			var rImage = $("<img>").attr("src", recipeOne.image).attr("alt", recipeOne.title).attr("target", "_blank").attr("data-ID", recipeOne.unique).css("cursor", "pointer");
+		    			rImage.addClass("rec-image");
+		    			$(".recipeImageOne").append(rImage);
+		    			$(".descriptionOne").append(recipeOne.description);
 					});
 					
 
@@ -114,76 +123,82 @@ $( document ).ready(function() {
 		    		}
 
 		    		console.log(recipeTwo);
+		    			$("#recipeTwo").text(recipeTwo.title);
+		    			$("#authorTwo").text("Added By: " + recipeTwo.author);
+		    			var rImage = $("<img>").attr("src", recipeTwo.image).attr("alt", recipeTwo.title).attr("target", "_blank").attr("data-ID", recipeTwo.unique).css("cursor", "pointer");
+		    			rImage.addClass("rec-image");
+		    			$(".recipeImageTwo").append(rImage);
+		    			$(".descriptionTwo").append(recipeTwo.description);
 					});
 					
 
-				//ajax four
+				// //ajax four
 
-				$.ajax({
-					url:"https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/extract?forceExtraction=false&url=" + edamamThreeUrl,
-					method: "GET",
-					headers: {
-				        "X-Mashape-Key":mashApeKey,
-				        "X-Mashape-Host":mashApeHost,
-				    }
-				    }).done(function (result) {
-				   	 console.log(result);
-				   	var recipeThree = {
-			    		title: response.hits[3].recipe.label,
-			    		author: response.hits[3].recipe.source,
-			    		description: result.instructions,
-			    		image: response.hits[3].recipe.image,
-			    		unique: result.id
-		    		}
+				// $.ajax({
+				// 	url:"https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/extract?forceExtraction=false&url=" + edamamThreeUrl,
+				// 	method: "GET",
+				// 	headers: {
+				//         "X-Mashape-Key":mashApeKey,
+				//         "X-Mashape-Host":mashApeHost,
+				//     }
+				//     }).done(function (result) {
+				//    	 console.log(result);
+				//    	var recipeThree = {
+			 //    		title: response.hits[3].recipe.label,
+			 //    		author: response.hits[3].recipe.source,
+			 //    		description: result.instructions,
+			 //    		image: response.hits[3].recipe.image,
+			 //    		unique: result.id
+		  //   		}
 
-		    		console.log(recipeThree);
-					});
+		  //   		console.log(recipeThree);
+				// 	});
 					
 
-				//ajax five
+				// //ajax five
 
-				$.ajax({
-					url:"https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/extract?forceExtraction=false&url=" + edamamFourUrl,
-					method: "GET",
-					headers: {
-				        "X-Mashape-Key":mashApeKey,
-				        "X-Mashape-Host":mashApeHost,
-				    }
-				    }).done(function (result) {
-				   	 console.log(result);
-				   	var recipeFour = {
-			    		title: response.hits[4].recipe.label,
-			    		author: response.hits[4].recipe.source,
-			    		description: result.instructions,
-			    		image: response.hits[4].recipe.image,
-			    		unique: result.id
-		    		}
+				// $.ajax({
+				// 	url:"https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/extract?forceExtraction=false&url=" + edamamFourUrl,
+				// 	method: "GET",
+				// 	headers: {
+				//         "X-Mashape-Key":mashApeKey,
+				//         "X-Mashape-Host":mashApeHost,
+				//     }
+				//     }).done(function (result) {
+				//    	 console.log(result);
+				//    	var recipeFour = {
+			 //    		title: response.hits[4].recipe.label,
+			 //    		author: response.hits[4].recipe.source,
+			 //    		description: result.instructions,
+			 //    		image: response.hits[4].recipe.image,
+			 //    		unique: result.id
+		  //   		}
 
-		    		console.log(recipeFour);
-					});
+		  //   		console.log(recipeFour);
+				// 	});
 					
 
-				//ajax six
+				// //ajax six
 
-				$.ajax({
-					url:"https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/extract?forceExtraction=false&url=" + edamamFiveUrl,
-					method: "GET",
-					headers: {
-				        "X-Mashape-Key":mashApeKey,
-				        "X-Mashape-Host":mashApeHost,
-				    }
-				    }).done(function (result) {
-				   	 console.log(result);
-				   	var recipeFive = {
-			    		title: response.hits[5].recipe.label,
-			    		author: response.hits[5].recipe.source,
-			    		description: result.instructions,
-			    		image: response.hits[5].recipe.image,
-			    		unique: result.id
-		    		}
+				// $.ajax({
+				// 	url:"https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/extract?forceExtraction=false&url=" + edamamFiveUrl,
+				// 	method: "GET",
+				// 	headers: {
+				//         "X-Mashape-Key":mashApeKey,
+				//         "X-Mashape-Host":mashApeHost,
+				//     }
+				//     }).done(function (result) {
+				//    	 console.log(result);
+				//    	var recipeFive = {
+			 //    		title: response.hits[5].recipe.label,
+			 //    		author: response.hits[5].recipe.source,
+			 //    		description: result.instructions,
+			 //    		image: response.hits[5].recipe.image,
+			 //    		unique: result.id
+		  //   		}
 
-		    		console.log(recipeFive);
-					});
+		  //   		console.log(recipeFive);
+				// 	});
 				})
 
 
