41e55bcfece07c3beab31bb45f655c917746898d
Jacob Hershberger <whyisjacob@gmail.com>
2017-11-14 21:29:42 -0700

    updated various stuff

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -18,32 +18,42 @@ jQuery(document).ready(function() {
 		}
 	});
 
-	$(document).on('click','li', function(){
+	$(document).on('click','.ing_display',function(){
+		$(this).children('.glyphicon').toggleClass('glyphicon-remove-circle')
+		$(this).children('.glyphicon').toggleClass(' glyphicon-ok-circle')
+	})
+		
+
+	/*$(document).on('click','li', function(){
 
 		//I'd like to give the unordered lists a data-attribute with a value that represents the ingredient listed inside. Ingredient by itself, not including the measurement, etc. Adding the nutritional value by presented measurement could be an interesting little math project.
-	//edamam api data & converting JSONP into JSON
-	console.log(this);
-		var query = $(this).val().trim();
-		console.log(query);
-		var appId = "c26ba31b";
-		var apiKey = "0e164b393b7346efa7e6769658022a14";
-		var queryUrl = "https://api.edamam.com/search?q=" + query + "&app_id=" + appId + "&app_key=" + apiKey + "&from=0&to=6&callback=myfunc";
-		var myfunc = function (json) {
-		  alert(json);
-			}
-		$.ajax({
-		     url: queryUrl,
-		     method: "GET",
-		     dataType: 'jsonp', 
-		     jsonpCallback: 'myfunc', 
-		 	 jsonp: 'callback'
- 	 
-	    }).done(function(response) {
-    		console.log(response);
-    		console.log(response.hits[0]);
-    		console.log(response.hits[0].recipe.calories);
+		console.log(this);
+		$(this).removeClass('glyphicon-remove-circle')
+		$(this).addClass('glyphicon glyphicon-ok-circle')
 
-		})
+		//edamam api data & converting JSONP into JSON
+
+		// var query = $(this).val().trim();
+		// console.log(query);
+		// var appId = "c26ba31b";
+		// var apiKey = "0e164b393b7346efa7e6769658022a14";
+		// var queryUrl = "https://api.edamam.com/search?q=" + query + "&app_id=" + appId + "&app_key=" + apiKey + "&from=0&to=6&callback=myfunc";
+		// var myfunc = function (json) {
+		//   alert(json);
+		// 	}
+		// $.ajax({
+		//      url: queryUrl,
+		//      method: "GET",
+		//      dataType: 'jsonp', 
+		//      jsonpCallback: 'myfunc', 
+		//  	 jsonp: 'callback'
+ 	 
+	    // }).done(function(response) {
+    	// 	console.log(response);
+    	// 	console.log(response.hits[0]);
+    	// 	console.log(response.hits[0].recipe.calories);
+
+		// })
 	});
 
 	/**
